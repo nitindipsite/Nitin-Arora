e8d026caf1336013a8655591d171a4c38d4d131f
nitindipsite <nitindipsite@live.com>
2015-02-04 07:38:07 +1300

    FilterActionAttr, Global filters

---
+++ MVC Filters/Filters/Controllers/CustomerController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Filters.Controllers
+{
+    public class CustomerController : Controller
+    {
+        //
+        // GET: /Customer/
+
+        // Because ProfileAll is now registered in the global filter collection, it gets applied to all the actions
+        // automatically.
+        public string Index()
+        {
+            return "This is the customer controller";
+        }
+
+    }
+}
