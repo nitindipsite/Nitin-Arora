260e3170e32e1e47b1cee706fb669ef65a740f34
Nitin Arora <nitindipsite@live.com>
2015-02-04 10:08:49 +1300

    angularJS ng-include AND filter changes

--- MVC Filters/Filters/Controllers/HomeController.cs
+++ MVC Filters/Filters/Controllers/HomeController.cs
@@ -11,12 +11,14 @@ namespace Filters.Controllers
     {
         //
         // GET: /Home/
+        // 1. Testing custom authorization attribute.
         [CustomAuthorizeAtrribute(false)]
         public string Index()
         {
             return "This is Index() action on the Home controller";
         }
 
+        // 2a. Testing custom error attribute.
         [RangeException]
         public string RangeTest(int id)
         {
@@ -30,10 +32,34 @@ namespace Filters.Controllers
             }
         }
 
+        // 2b. Testing built in HandleError attribute to handle exceptions.
+        [HandleError(ExceptionType = typeof(ArgumentOutOfRangeException), View = "RangeError")]
+        public string RangeTestDefault(int id)
+        {
+            if (id > 100)
+            {
+                return String.Format("The id value is: {0}", id);
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException("id", id, "");
+            }
+        }
+
+        // 3 and 4: Using IActionFilter and IResultFilter
+        [ProfileAction]
+        [ProfileResult]
+        public string ActionFilterTest()
+        {
+            return @"This is ActionFilter Test";
+        }
+
+        // Same as above but done using inheriting ActionFilterAttribute
         [ProfileAll]
         public string FilterTest()
         {
             return "This is a filter test action";
         }
+
     }
 }
