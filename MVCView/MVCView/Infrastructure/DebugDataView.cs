db4b56f5dea491a7d41be9959cecb53d5ba44211
Nitin Arora <nitindipsite@live.com>
2015-02-10 14:22:09 +1300

    Uploading MVC View..How to extend view engine

---
+++ MVCView/MVCView/Infrastructure/DebugDataView.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+
+namespace MVCView.Infrastructure
+{
+    public class DebugDataView : IView
+    {
+        public void Render(ViewContext viewContext, TextWriter writer)
+        {
+            Write(writer, "---ROUTING DATA---");
+            foreach (string key in viewContext.RouteData.Values.Keys)
+            {
+                Write(writer, "Key: {0}, Value: {1}", key, viewContext.RouteData.Values[key]);
+            }
+
+            Write(writer, "---VIEW DATA---");
+            foreach (string key in viewContext.ViewData.Keys)
+            {
+                Write(writer, "Key: {0}, Value: {1}", key, viewContext.ViewData[key]);
+            }
+
+        }
+
+        private void Write(TextWriter writer, string template, params object[] values)
+        {
+            writer.Write(string.Format(template, values) + "<p/>");
+        }
+    }
+}
