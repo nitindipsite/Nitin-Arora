f27fd0049055f5b6a83c2a34350b605f6c89d1b5
Nitin Arora <nitindipsite@live.com>
2015-02-10 15:20:50 +1300

    Showing how @sections work

---
+++ MVCView/MVCView/Infrastructure/CustomRazorViewEngine.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCView.Infrastructure
+{
+    // Inheriting from RazorViewEngine to change the location of where MVC looks for
+    // views.
+    public class CustomRazorViewEngine : RazorViewEngine
+    {
+        public CustomRazorViewEngine()
+        {
+            ViewLocationFormats = new string[] { "/Views/{1}/{0}.cshtml", "/Views/Common/{0}.cshtml" };
+        }
+    }
+}
